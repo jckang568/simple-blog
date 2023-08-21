c57cf4666c70b2edb68808d1a2dd2be795c3c9d2
jongchan <jckang568@gmail.com>
2023-08-21 18:07:33 +0900

    add an annotation unused dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,20 +24,20 @@ extra["snippetsDir"] = file("build/generated-snippets")
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-cache")
+//    implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+//    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+//    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
+//    implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-    testImplementation("org.springframework.security:spring-security-test")
+//    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+//    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
@@ -52,10 +52,10 @@ tasks.withType<Test> {
 }
 
 tasks.test {
-    outputs.dir(snippetsDir)
+//    outputs.dir(snippetsDir)
 }
 
-tasks.asciidoctor {
-    inputs.dir(snippetsDir)
-    dependsOn(test)
-}
+//tasks.asciidoctor {
+//    inputs.dir(snippetsDir)
+//    dependsOn(test)
+//}
